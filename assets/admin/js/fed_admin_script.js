cf8bc8d8fc1cef4aa533962bbce7fe953aa8d806
Vinoth Kumar <ma.vinothkumar@gmail.com>
2026-09-11 16:46:07 +0530

    V3 - Form Fields

--- assets/admin/js/fed_admin_script.js
+++ assets/admin/js/fed_admin_script.js
@@ -1399,5 +1399,63 @@ jQuery.fed_toggle_loader = function ($) {
 		updateBuilderState( $( this ).closest( '.fed-choices-builder' ) );
 	} );
 
+	// Color picker: Native color input change/input
+	$( document ).on( 'input change', '.fed_color_native', function () {
+		var hex = $( this ).val().toUpperCase();
+		var container = $( this ).closest( '.fed_color_picker_container' );
+		container.find( '.fed_color_swatch' ).css( 'background-color', hex );
+		var input = container.find( '.fed_color_input' );
+		if ( input.val() !== hex ) {
+			input.val( hex ).trigger( 'change' );
+		}
+	} );
+
+	// Color picker: Text input typing/pasting
+	$( document ).on( 'input', '.fed_color_input', function () {
+		var val = $( this ).val().trim();
+		if ( ! val ) return;
+		if ( val.charAt( 0 ) !== '#' ) {
+			val = '#' + val;
+		}
+		var container = $( this ).closest( '.fed_color_picker_container' );
+		var valid3 = /^#([0-9A-Fa-f]{3})$/;
+		var valid6 = /^#([0-9A-Fa-f]{6})$/;
+		var fullHex = '';
+		if ( valid6.test( val ) ) {
+			fullHex = val.toUpperCase();
+		} else if ( valid3.test( val ) ) {
+			var r = val.charAt( 1 ), g = val.charAt( 2 ), b = val.charAt( 3 );
+			fullHex = ( '#' + r + r + g + g + b + b ).toUpperCase();
+		}
+		if ( fullHex ) {
+			container.find( '.fed_color_swatch' ).css( 'background-color', fullHex );
+			container.find( '.fed_color_native' ).val( fullHex.toLowerCase() );
+		}
+	} );
+
+	// Color picker: Text input blur formatting
+	$( document ).on( 'blur', '.fed_color_input', function () {
+		var val = $( this ).val().trim();
+		var container = $( this ).closest( '.fed_color_picker_container' );
+		if ( ! val ) return;
+		if ( val.charAt( 0 ) !== '#' ) {
+			val = '#' + val;
+		}
+		var valid3 = /^#([0-9A-Fa-f]{3})$/;
+		var valid6 = /^#([0-9A-Fa-f]{6})$/;
+		if ( valid6.test( val ) ) {
+			var upper = val.toUpperCase();
+			$( this ).val( upper );
+			container.find( '.fed_color_swatch' ).css( 'background-color', upper );
+			container.find( '.fed_color_native' ).val( val.toLowerCase() );
+		} else if ( valid3.test( val ) ) {
+			var r = val.charAt( 1 ), g = val.charAt( 2 ), b = val.charAt( 3 );
+			var full = ( '#' + r + r + g + g + b + b ).toUpperCase();
+			$( this ).val( full );
+			container.find( '.fed_color_swatch' ).css( 'background-color', full );
+			container.find( '.fed_color_native' ).val( full.toLowerCase() );
+		}
+	} );
+
 } )( jQuery );
 
